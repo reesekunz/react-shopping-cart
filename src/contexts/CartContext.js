8c94b8eacf6a10c0419071b7f01759a5f2d785e6
Reese Kunz <reesekunz21@gmail.com>
2019-08-12 12:33:49 -0700

    created cart context

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,7 @@
+import React, { createContext } from "react";
+
+export const CartContext = createContext();
+console.log(CartContext);
+console.log(CartContext.Provider)
+console.log(CartContext.Consumer)
+
