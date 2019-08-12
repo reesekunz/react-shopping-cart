ebd3609fe8ea68f40be715eb69df3cf06795bfca
Reese Kunz <reesekunz21@gmail.com>
2019-08-12 12:40:33 -0700

    shoppingcart and navigation components now getting data from cart context

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
+
 
 function App() {
 	const [products] = useState(data);
@@ -19,6 +21,8 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
+					<CartContext.Provider value={{ cart }}>
+
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -30,6 +34,7 @@ function App() {
 				render={() => <ShoppingCart cart={cart} />}
 			/>
 		</div>
+		</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 	
